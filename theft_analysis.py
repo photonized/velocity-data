ab6b4064a6aebe2f762048db707f29567e10bdac
Vlad <vlad.j.work@gmail.com>
2020-09-14 14:30:27 -0400

    data progress and my mapbox token
    for some reason mapbox refuses to open in the notebook lol

--- theft_analysis.py
+++ theft_analysis.py
@@ -1,7 +1,14 @@
 import pandas as pd
+import numpy as np
+import plotly.express as px
 
 theft_df = pd.read_csv('./Bicycle_Theft.csv')
 
-cost_sum = theft_df['Bicycle_value'].sum() # 3678514.0
+cost_sum = theft_df['Bicycle_Value'].sum()  # 3678514.0
 
-total_thefts = theft_df['ID'].count() # 5652
+total_thefts = theft_df['ID'].count()  # 5652
+
+px.set_mapbox_access_token("pk.eyJ1IjoidmxhZGpkayIsImEiOiJja2YydWRiN2YwMjV6MnVydjd5NzQzdXJ2In0.FK1eN7UGZkrHj7qTdz_sBA")
+fig = px.scatter_mapbox(theft_df, lat="y", lon="x",
+                        color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
+fig.show()
